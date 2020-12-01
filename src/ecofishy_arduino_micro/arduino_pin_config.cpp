f6b7761984b57a180d855853473a06cf02e26d92
Ryota <ryota901024@gmail.com>
2020-12-01 23:32:35 +0900

    replace macro

--- src/ecofishy_arduino_micro/arduino_pin_config.cpp
+++ src/ecofishy_arduino_micro/arduino_pin_config.cpp
@@ -9,6 +9,7 @@
  * 
  ****************************************************************************/
 #include "arduino_pin_config.h"
+#include "LED_lib.h"
 #ifndef UNIT_TEST
   #include <Arduino.h>
   #include <pins_arduino.h>
@@ -26,9 +27,9 @@ static const ARDUINO_PIN_T s_init_pin_conf[] = {
     {(uint8_t)PIN_UART_RX,      (uint8_t)INPUT_PULLUP, (uint8_t)NULL},  // RX of BP35A1
     {(uint8_t)PIN_BP35A1_WKUP,  (uint8_t)OUTPUT,       (uint8_t)HIGH},  // BP35A1 WKUP
     {(uint8_t)PIN_BP35A1_RESET, (uint8_t)INPUT_PULLUP, (uint8_t)NULL},  // BP35A1 RESET
-    {(uint8_t)PIN_LED_RED,      (uint8_t)OUTPUT,       (uint8_t)255},  // LED R
-    {(uint8_t)PIN_LED_BLU,      (uint8_t)OUTPUT,       (uint8_t)255},  // LED G
-    {(uint8_t)PIN_LED_GRN,      (uint8_t)OUTPUT,       (uint8_t)255},  // LED B
+    {(uint8_t)PIN_LED_RED,      (uint8_t)OUTPUT,       (uint8_t)MIN_LED_BRIGHTNESS},  // LED R
+    {(uint8_t)PIN_LED_BLU,      (uint8_t)OUTPUT,       (uint8_t)MIN_LED_BRIGHTNESS},  // LED G
+    {(uint8_t)PIN_LED_GRN,      (uint8_t)OUTPUT,       (uint8_t)MIN_LED_BRIGHTNESS},  // LED B
     {(uint8_t)PIN_SPI_MISO,     (uint8_t)INPUT_PULLUP, (uint8_t)NULL},  // MISO
     {(uint8_t)PIN_SPI_SCLK,     (uint8_t)INPUT_PULLUP, (uint8_t)NULL},  // SCLK
     {(uint8_t)PIN_SPI_MOSI,     (uint8_t)INPUT_PULLUP, (uint8_t)NULL},  // MOSI
